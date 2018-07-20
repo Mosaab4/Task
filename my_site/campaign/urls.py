fef3b1ffa51df3db5c48ef2f2d55e3204dd7574c
mosaab <musab.ehab@gmail.com>
2018-07-20 19:23:01 +0200

    add update view

--- my_site/campaign/urls.py
+++ my_site/campaign/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
-from .views import CampaignListView, AddView
+from .views import CampaignListView, AddView,UpdateView
 
 app_name = 'campaign'
 
 urlpatterns = [
     path('', CampaignListView.as_view(), name='list'),
     path('add/', AddView.as_view(), name='add'),
+    path('update/<int:pk>/',UpdateView.as_view(),name='update'),
 ]
--- my_site/campaign/views.py
+++ my_site/campaign/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
-from django.views.generic import ListView, FormView
 from django.shortcuts import redirect
+from django.views.generic import (
+    ListView, 
+    FormView,
+    UpdateView,
+    DeleteView
+)
+
+
 
 from .models import Campaign
 from .forms import CampaignForm
@@ -18,7 +25,20 @@ class AddView(FormView):
     model = Campaign
 
     def form_valid(self,form):
-        self.campaing = form.save(commit=False)
-        self.campaing.save()
+        self.campaign = form.save(commit=False)
+        self.campaign.save()
 
         return redirect('campaign:list')
+
+class UpdateView(UpdateView):
+    model = Campaign
+    template_name = 'campaign/add.html'
+    form_class = CampaignForm
+    pk_url_kwarg = 'pk'
+
+    def form_valid(self,form):
+        self.campaign = form.save(commit=False)
+        self.campaign.save()
+
+        return redirect('campaign:list')
+
